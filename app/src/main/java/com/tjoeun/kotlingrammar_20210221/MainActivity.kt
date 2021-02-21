030c623d283a82bf1e08bbba3b42f44407b4c876
seoyeongkang <jumprope123@naver.com>
2021-02-21 10:01:28 +0900

    변수에 담긴 강사 이름 로그로 출력

--- app/src/main/java/com/tjoeun/kotlingrammar_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/kotlingrammar_20210221/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.kotlingrammar_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +22,15 @@ class MainActivity : AppCompatActivity() {
 //        num2 = 20 // num2는 value (상수). 한번 대입하고 나면 변경 불가
 
 
-//        Int, Double, String Boolean 자료형 자동 대입 예제
+//        Int, Double, String, Boolean 자료형 자동 대입 예제
 
         val num3 = 30 // num3는 30 Int 니까 자동으로 Int로 설정.
         val num4 = 3.14 // num4는 3.14가 Double 이므로 알아서 Double로 설정.
         val teacherName = "조경진" //String 자동 할당
         val isMale = true // Boolean 자동 할당
 
+//        변수에 담긴 강사 이름 로그로 출력
+        Log.d("강사이름", teacherName)
 
 
 
