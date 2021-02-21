10ecf4477912459b8ba7909b74d782bba6dd78b1
seoyeongkang <jumprope123@naver.com>
2021-02-21 09:50:18 +0900

    var / var 사용 예시

--- app/src/main/java/com/tjoeun/kotlingrammar_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/kotlingrammar_20210221/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        변수로 => 정수를 담을 공간. JAVA : int num1;
+        var num1 : Int
+
+//        변수에 값 대입
+        num1 = 10
+        num1 = 20
+
+//        상수로 => 정수를 담을 공간. JAVA : final int num2;
+        val num2 : Int
+        num2 = 10
+//        num2 = 20 // num2는 value (상수). 한번 대입하고 나면 변경 불가
+        
+
+
     }
 }
